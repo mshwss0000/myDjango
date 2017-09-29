f7fc1ac66a94e104ed05787617087e6d05a8ba79
mshwss0000 <464875600@qq.com>
2017-09-29 19:42:15 +0900

    18.5

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     # 主页
     url(r'^$', views.index, name='index'),
+    # 显示所有的主题
+    url(r'^topics/$', views.topics, name='topics'),
+    # 特定主题的详细页面
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
